bdd25b54e49d6f6ec6873ae05aa43bfbe964ad1f
HumberZam <humber_zam@yahoo.com.mx>
2022-02-27 19:14:30 -0600

    Se agregan variables Ruby.

--- snmp/manifests/init.pp
+++ snmp/manifests/init.pp
@@ -1,20 +1,30 @@
-Package['net-snmp'] -> File['/etc/snmp/snmpd.conf'] ~> Service['snmpd']
- 
+
 class snmp {
-  package { 'net-snmp':
+
+  Package['snmp'] -> File['snmpd.conf'] ~> Service['snmp']
+
+  $package    = 'net-snmp'
+  $configfile = '/etc/snmp/snmpd.conf'
+  $service    = 'snmpd'
+
+  package { 'snmp':
     ensure => installed,
+    name   => "${package}",
   }
 
-  file { '/etc/snmp/snmpd.conf':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0444',
-    source => 'puppet:///modules/snmp/snmpd.conf',
+# Agregar la linea: content => template('snmp/snmpd.conf'), hace que puppet busque en la carpeta: template.
+  file { 'snmpd.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0444',
+    content => template('snmp/snmpd.conf.erb'),
+    path    => "${configfile}",
   }
 
-  service { 'snmpd':
+  service { 'snmp':
     ensure => running,
+    name   => "${service}",
   }
 
 }
